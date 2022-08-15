2109ba7370adc428081e21a0344d5544eeefa964
HernanRT <106358914+HernanRT@users.noreply.github.com>
2022-08-14 21:26:43 -0300

    Update Valoracionfinalpython.py

--- Valoracionfinalpython.py
+++ Valoracionfinalpython.py
@@ -1,3 +1,5 @@
-dueño = [28957346, "Juan", "Perez", 4789689, "Belgrano 101"] 
-if dueño[0] > 26000000 :
-    print("DNI: ", dueño[0])
+#Actividad N° 1 de Python
+
+dueno = [28957346, "Juan", "Perez", 4789689, "Belgrano 101"] 
+if dueno[0] > 26000000 :
+    print("Teléfono: ", dueno[3])
